60dc143388ff765dc19c312d2bd83a64b949f865
Marcelo Corpucci <mcorpucci@gmail.com>
2020-02-16 21:10:05 -0300

    basic test case implemented.

--- api/test/test_engine.py
+++ api/test/test_engine.py
@@ -2,7 +2,12 @@ import pytest
 from api.main import create_app
 
 
-def test_mongo_connection():
-    # app = create_app(config_name="testing")
-    # client = app.test_client
-    pass
+def test_basic_engine_creation():
+    app = create_app(profile="testing")
+
+    with app.test_client() as client:
+        response = client.post('/api/engine', json={
+            "model": "testD", "displacement": 1400
+        })
+
+        assert response.status_code == 201
