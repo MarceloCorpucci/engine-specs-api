d0f66fd39d21cfc2a79c76e091fc8f3fcb70b88e
Marcelo Corpucci <mcorpucci@gmail.com>
2020-04-01 09:06:11 -0300

    Added get to ECU routes.

--- api/routes/ecu_routes.py
+++ api/routes/ecu_routes.py
@@ -20,23 +20,31 @@ ecu_api = Blueprint('ecu_api', __name__, url_prefix='/api/ecu')
 @ecu_api.route('/', methods=['POST'])
 @jwt_required
 def create_ecu():
-	try:
-		data = request.get_json()
-		logging.info('create_ecu() --> Received data: ' + str(data))
-		json_data = json.loads(str(data).replace('\'', '\"'))
+    try:
+        data = request.get_json()
+        logging.info('create_ecu() --> Received data: ' + str(data))
+        json_data = json.loads(str(data).replace('\'', '\"'))
 
-		ecu_to_save = Ecu(model=json_data['model'],
-						  firmware=json_data['firmware'],
-						  date_added=json_data['date_added'])
+        ecu_to_save = Ecu(model=json_data['model'],
+                          firmware=json_data['firmware'],
+                          date_added=json_data['date_added'])
 
-		ecu_to_save.engine = Engine.objects.get(model=json_data['engine']['model'])
-		ecu_to_save.warning_preset = WarningPreset.objects.get(name=json_data['warning_preset']['name'])
-		ecu_to_save.user = User.objects.get(email=json_data['user']['email'])
+        ecu_to_save.engine = Engine.objects.get(model=json_data['engine']['model'])
+        ecu_to_save.warning_preset = WarningPreset.objects.get(name=json_data['warning_preset']['name'])
+        ecu_to_save.user = User.objects.get(email=json_data['user']['email'])
 
-		ecu_to_save.save()
+        ecu_to_save.save()
 
-		return response_with(resp.SUCCESS_201)
+        return response_with(resp.SUCCESS_201)
 
-	except Exception as e:
-		logging.error(e)
-		return response_with(resp.INVALID_INPUT_422)
+    except Exception as e:
+        logging.error(e)
+        return response_with(resp.INVALID_INPUT_422)
+
+
+@ecu_api.route('/', methods=['GET'])
+def get_ecu():
+    ecu = Ecu.objects.all()
+    logging.info('get_ecu() --> Retrieving data: ' + str(ecu))
+
+    return make_response(jsonify({'ecu': ecu}))
